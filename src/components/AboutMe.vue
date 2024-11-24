b4d195f6b0cafa933e3d8673de175153fe8c2662
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2024-11-23 16:20:06 -0800

    Update sections
    - Add header and 2 column layout
    - Update basic data and details

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <img alt="Profile" src="../assets/images/profile.JPG" />
+
+    <div>
+      <h1>Gurkirat Sidhu</h1>
+      <h2>Full Stack Developer</h2>
+
+      <h3>📍 Burnaby, BC</h3>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+img {
+  height: 120px;
+  width: 120px;
+  border-radius: 50%;
+}
+
+header {
+  padding: 8%;
+  position: fixed;
+  left: 0;
+  right: 60%;
+  top: 0;
+  display: flex;
+  gap: 8px;
+  align-items: flex-start;
+  flex-direction: column;
+}
+</style>
